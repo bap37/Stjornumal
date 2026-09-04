e4dbd3acacd8e87b0dc074b9680b00533edcc3d9
Brodie <bap37@duke.edu>
2026-09-04 11:34:40 +0100

    output can now be saved into a CSV file

--- dustbi_simulator.py
+++ dustbi_simulator.py
@@ -1134,6 +1134,12 @@ def simulate_model(n_sim, n_batch, sims_savename, priors, simulator, inference, 
 
     return theta_valid, p_vals
 
+def save_output(labels, theta_hat, filename):
+    with open(filename, "w") as f:
+        for i in range(len(labels)):
+            string = f"{labels[i]},{theta_hat[i]:.4f}"
+            string = string+"\n"
+            f.write(string)
 
 
 
